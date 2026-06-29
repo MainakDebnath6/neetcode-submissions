7d6576207ce6f499665d167e04087f5ab91daea6
Mainak Debnath <143885385+MainakDebnath6@users.noreply.github.com>
2026-06-29 09:06:33 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        for(int s:stones){
+            minHeap.offer(-s);
+        }
+        while(minHeap.size()>1){
+            int first=minHeap.poll();
+            int second=minHeap.poll();
+            if(second>first){
+                minHeap.offer(first-second);
+            }
+        }
+        return minHeap.isEmpty()?0:-minHeap.peek();
+    }
+}
