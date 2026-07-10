53f20f28ad2f60046ea9ecacd8ae8d876d022011
Mainak Debnath <143885385+MainakDebnath6@users.noreply.github.com>
2026-07-10 11:35:07 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count=new int[26];
+        for(char c:tasks){
+            count[c-'A']++;
+        }
+        int maxf=Arrays.stream(count).max().getAsInt();
+        int maxCount=0;
+        for(int i:count){
+            if(i==maxf) maxCount++;
+        }
+        int time=(maxf-1)*(n+1) +maxCount;
+        return Math.max(time,tasks.length);
+    }
+}
