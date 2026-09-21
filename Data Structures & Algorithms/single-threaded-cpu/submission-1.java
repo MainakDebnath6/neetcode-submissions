5f1f828deb9357a713367116cb7343f3f1565fdf
Mainak Debnath <143885385+MainakDebnath6@users.noreply.github.com>
2026-09-21 09:12:39 +0530

    Add: single-threaded-cpu - submission-1

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] getOrder(int[][] tasks) {
+        int n=tasks.length;
+        for(int i=0;i<n;i++){
+            tasks[i]=new int[]{tasks[i][0],tasks[i][1],i};
+        }
+        Arrays.sort(tasks,Comparator.comparingInt(t->t[0]));
+        int[] res=new int[n];
+        PriorityQueue<int[]> mh=new PriorityQueue<>((a,b)->a[0]==b[0]?Integer.compare(a[1],b[1]):Integer.compare(a[0],b[0]));
+        int i=0,idx=0;
+        long time=tasks[0][0];
+        while(!mh.isEmpty() || i<n){
+            while(i<n && time>=tasks[i][0]){
+                mh.offer(new int[]{tasks[i][1],tasks[i][2]});
+                i++;
+            }
+            if(mh.isEmpty()){
+                time=tasks[i][0];
+            }else{
+                int[] task=mh.poll();
+                time+=task[0];
+                res[idx++]=task[1];
+            }
+        }
+        return res;
+    }
+}
