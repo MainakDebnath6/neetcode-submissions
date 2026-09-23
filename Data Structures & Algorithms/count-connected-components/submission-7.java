d13fc5709d4a3928821b576717d62a5e7c365647
Mainak Debnath <143885385+MainakDebnath6@users.noreply.github.com>
2026-09-23 11:18:01 +0530

    Add: count-connected-components - submission-7

---
+++ Data Structures & Algorithms/count-connected-components/submission-7.java
@@ -0,0 +1,41 @@
+class DSU{
+    int[] Parent,Rank;
+    public DSU(int n){
+        Parent=new int[n+1];
+        Rank=new int[n+1];
+        for(int i=0;i<=n;i++){
+            Parent[i]=i;
+            Rank[i]=1;
+        }
+    }
+    public int find(int node){
+        if(Parent[node]!=node){
+            Parent[node]=find(Parent[node]);
+        }
+        return Parent[node];
+    }
+    public boolean union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv) return false;
+        if(Rank[pv]>Rank[pu]){
+            int tmp=Rank[pu];
+            Rank[pu]=Rank[pv];
+            Rank[pv]=tmp;
+        } 
+        Rank[pu]+=Rank[pv];
+        Parent[pv]=pu;
+        return true;
+    }
+}
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        DSU dsu=new DSU(n);
+        int res=n;
+        for(int[] edge:edges){
+            if(dsu.union(edge[0],edge[1])) res--;
+        }
+        return res;
+    }
+}
